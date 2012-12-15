ff3584d533f3fa7a52650222309a646c0a420da2
c0bra61 <c0bra@xiatek.org>
2012-12-15 14:23:12 +0000

    First

---
+++ gamemode/init.lua
@@ -0,0 +1,21 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+
+include("shared.lua")
+
+function GM:PlayerSpawn(pl)
+    self.BaseClass:PlayerSpawn(pl)
+end
+
+function GM:PlayerInitialSpawn(pl)
+	self.BaseClass:PlayerInitialSpawn(pl)
+end
+
+ 
+function GM:PlayerLoadout(pl)
+	self.BaseClass:PlayerLoadout(pl)
+	
+	pl:Give("weapon_physcannon")
+	pl:Give("weapon_physgun")
+	pl:Give("gmod_tool")
+end
---
+++ gamemode/shared.lua
@@ -0,0 +1,6 @@
+GM.Name		 = "Endure It 2"
+GM.Author	 = "C0BRA"
+GM.Email	 = "c0bra@xiatek.org"
+GM.Website	 = "xiatek.org"
+
+DeriveGamemode("sandbox")
